935fdc78d8cfed054e69c11233718d74e47f647c
Symeon Giazitzis <simosgiaz@gmail.com>
2025-01-19 17:45:40 +0100

    Adjustments to the homepage

--- front-end/src/components/HomePage.vue
+++ front-end/src/components/HomePage.vue
@@ -1,11 +1,13 @@
 <template>
-  <div>
-    <h1>Welcome to the Tax Calculator</h1>
-    <p>
-      This tool helps you calculate your taxes efficiently. Navigate to the tax
-      form to get started.
+  <div class="home-page">
+    <h1>My Tax Calculator</h1>
+    <p class="text1">
+      Calculate your taxes fast and efficiently. Get suggestions and advice from
+      our AI powered advisor.
     </p>
-    <router-link to="/tax-form">Go to Tax Form</router-link>
+    <p class="help-text">Navigate to the tax form to get started.</p>
+    <button><router-link to="/tax-form">Go to Tax Form</router-link></button>
+    <p class="signature">Made by Giazitzis Symeon</p>
   </div>
 </template>
 
@@ -16,11 +18,56 @@ export default {
 </script>
 
 <style scoped>
-h1 {
-  color: #333;
+.home-page {
+  background-color: rgb(172, 206, 190);
+  margin: auto;
+  width: 100vw;
+  height: 100vh;
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  text-align: center;
+  padding: 0px;
 }
-p {
+h1 {
+  font-size: 2.5em;
+  position: fixed;
+  top: 70px;
+  color: #333;
+  margin: 50px;
+}
+.text1 {
+  position: fixed;
+  font-size: 1.5em;
+  top: 300px;
+  color: #000000;
+}
+.help-text {
+  position: fixed;
+  font-size: 1.5em;
+  top: 400px;
+  color: #000000;
+}
+.signature {
+  font-size: 0.8em;
+  font-style: italic;
+  position: fixed;
+  right: 50px;
+  bottom: 20px;
+  color: #0e3606;
+}
+button {
+  background-color: #55a713;
+  position: fixed;
+  color: white;
   font-size: 1.2em;
-  margin: 10px 0;
+  padding: 10px 20px;
+  border: none;
+  border-radius: 5px;
+  top: 480px;
+  cursor: pointer;
+}
+button:hover {
+  background-color: #0a6d17;
 }
 </style>
