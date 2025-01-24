093d1024dcac51ef13a090d0b24d8dda6fba6551
Symeon Giazitzis <simosgiaz@gmail.com>
2025-01-24 06:36:20 +0100

    more uploads

---
+++ front-end/vite.config.js
@@ -0,0 +1,24 @@
+import { fileURLToPath, URL } from "node:url";
+
+import { defineConfig } from "vite";
+import vue from "@vitejs/plugin-vue";
+import vueDevTools from "vite-plugin-vue-devtools";
+
+// https://vite.dev/config/
+export default defineConfig({
+  plugins: [vue(), vueDevTools()],
+  resolve: {
+    alias: {
+      "@": fileURLToPath(new URL("./src", import.meta.url)),
+    },
+  },
+  server: {
+    host: "localhost", // Use '0.0.0.0' to allow access from external devices
+    port: 8000, // Change this to your desired port
+  },
+  test: {
+    globals: true,
+    environment: "jsdom",
+    setupFiles: "./test/setup.js", // Add this line
+  },
+});
